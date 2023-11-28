f861a71b4f50a65f832d29fb3410bf5e6defb172
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-11-28 00:34:12 -0300

    Agrego tooltip para cada tecnologia

--- portafolio/src/components/About/Techstack.js
+++ portafolio/src/components/About/Techstack.js
@@ -2,43 +2,70 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { FaHtml5, FaCss3Alt } from 'react-icons/fa';
 import { DiJavascript1, DiReact, DiNodejs } from 'react-icons/di';
-import { SiPostgresql , SiMysql, SiRedux} from 'react-icons/si';
-
+import {
+  SiPostgresql,
+  SiMysql,
+  SiRedux,
+  SiSwagger,
+  SiMongodb,
+  SiJest,
+} from 'react-icons/si';
+import ReactTooltip from 'react-tooltip';
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' data-tip='JavaScript'>
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
-        <FaHtml5 />
-      </Col>
-      <Col xs={4} md={2} className='tech-icons'>
-        <FaCss3Alt />
-      </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' data-tip='Node.js'>
         <DiNodejs />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' data-tip='React'>
         <DiReact />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' data-tip='Redux'>
         <SiRedux />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' data-tip='MySQL'>
         <SiMysql />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' data-tip='PostgreSQL'>
         <SiPostgresql />
       </Col>
-        <Col
-          xs={4}
-          md={2}
-          className='tech-icons d-flex align-items-center justify-content-center'
-        >
-          <p style={{ fontSize: '45px' }}>Express.js</p>
-        </Col>
+      <Col xs={4} md={2} className='tech-icons' data-tip='MongoDB'>
+        <SiMongodb />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons d-flex align-items-center justify-content-center'>
+        <p style={{ fontSize: '45px' }} data-tip='Express.js'>
+          Express.js
+        </p>
+      </Col>
+      <Col xs={4} md={2} className='tech-icons' data-tip='Swagger'>
+        <SiSwagger />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons' data-tip='Jest'>
+        <SiJest />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons' data-tip='HTML5'>
+        <FaHtml5 />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons' data-tip='CSS3'>
+        <FaCss3Alt />
+      </Col>
+
+      {/* Contenedor de tooltip personalizado */}
+      <style>
+        {`
+          .custom-tooltip {
+            max-width: 200px; /* Ancho máximo del tooltip */
+            min-width: 50px; /* Ancho mínimo del tooltip */
+            white-space: nowrap; /* Evita el salto de línea */
+          }
+        `}
+      </style>
+
+      <ReactTooltip effect='solid' place='top' className='custom-tooltip' />
     </Row>
   );
 }
--- portafolio/src/components/About/Toolstack.js
+++ portafolio/src/components/About/Toolstack.js
@@ -7,26 +7,39 @@ import {
   SiVercel,
   SiInsomnia,
 } from "react-icons/si";
+import ReactTooltip from 'react-tooltip';
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Visual Studio Code">
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Postman">
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Insomnia">
         <SiInsomnia />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Slack">
         <SiSlack />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-tip="Vercel">
         <SiVercel />
       </Col>
+
+      {/* Contenedor de tooltip personalizado */}
+      <style>
+        {`
+          .custom-tooltip {
+            max-width: 200px; /* Ancho máximo del tooltip */
+            min-width: 50px; /* Ancho mínimo del tooltip */
+            white-space: nowrap; /* Evita el salto de línea */
+          }
+        `}
+      </style>
+
+      <ReactTooltip effect='solid' place='top' className='custom-tooltip' />
     </Row>
   );
 }
